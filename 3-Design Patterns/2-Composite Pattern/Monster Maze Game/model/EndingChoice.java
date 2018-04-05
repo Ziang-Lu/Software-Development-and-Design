f2a04f89f49c0aef5a66aa77d32d527d8fbbb6b3
Ziang Lu <ziangl@andrew.cmu.edu>
2018-04-05 17:03:09 -0400

    add Monster Maze Game

---
+++ 3-Design Patterns/2-Composite Pattern/Monster Maze Game/model/EndingChoice.java
@@ -0,0 +1,10 @@
+package model;
+
+public abstract class EndingChoice extends Choice {
+
+    /**
+     * Prints the outcome of this ending choice.
+     */
+    public abstract void printOutcome();
+
+}
